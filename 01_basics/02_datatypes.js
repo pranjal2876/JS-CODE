58dd4e7f92bc0f9c0fbd2ff7558d4173fbb4bfb2
pranjal2876 <pranjal2876@gmail.com>
2025-07-18 18:45:52 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using node js not browser
+
+let name = "Pranjal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint very large number in stock market and trading
+// string ""
+// boolean true/false
+// null standalone value
+// undefined variable is not defined
+// symbol uniqueness
+
+
+//object
+console.log(typeof "Pranjal") //string
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
