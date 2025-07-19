b042ec6f96119c8da7d6b05382119f048100dc70
pranjal2876 <pranjal2876@gmail.com>
2025-07-19 22:24:25 +0530

    conversionoperation

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -4,3 +4,19 @@ console.log(typeof score);
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33"-> 33
+// "33abc" -> NaN
+// true -  1; false - 0
+
+
+let isLoogedin = 1 // string mein khali chod do its false and name wagera likho then true
+
+let booleanIsLoggedIn = Boolean(isLoogedin)
+console.log(booleanIsLoggedIn); // true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); // "33"
+console.log( typeof stringNumber);
