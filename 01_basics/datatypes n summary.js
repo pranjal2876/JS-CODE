ffe7c67e4e1bfaa61fa2eba9b088d422b8a48c5d
pranjal2876 <pranjal2876@gmail.com>
2025-07-20 11:39:49 +0530

    data comparison

---
+++ 01_basics/datatypes n summary.js
@@ -0,0 +1,9 @@
+// PRIMITIVE DATA TYPE
+// String, Number, Boolean, Undefined, Null, Symbol, BigInt 7 TYPES
+
+const score = 100 
+
+
+
+// REFERENCE (NON PRIMITIVE DATA TYPE)
+// Object, Array, Function, Date, RegExp, Map, Set
