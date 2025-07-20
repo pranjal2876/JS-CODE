36fa118a0ef97aa810a2bed2fedf1850722a81c1
pranjal2876 <pranjal2876@gmail.com>
2025-07-20 09:52:08 +0530

    conversion

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -18,9 +18,39 @@ console.log(booleanIsLoggedIn); // true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber); // "33"
-console.log( typeof stringNumber);
+// console.log(stringNumber); // "33"
+// console.log( typeof stringNumber);
 
 
 
 //**************************** OPERATIONS
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Pranjal"
+
+let str3 = str1 + str2
+//console.log(str3); // hello Pranjal
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1"+ 2 + 2);
+//console.log(1 + 2 + "2");
+
+console.log(+true); //1 but if true+ then error
+console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++ 
+console.log(gameCounter); // 101
