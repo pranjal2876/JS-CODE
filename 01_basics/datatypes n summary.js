eec9b350d8833d79d02aa6b39718d71ade28bf79
pranjal2876 <pranjal2876@gmail.com>
2025-07-25 19:57:23 +0530

    summary

--- 01_basics/datatypes n summary.js
+++ 01_basics/datatypes n summary.js
@@ -2,8 +2,33 @@
 // String, Number, Boolean, Undefined, Null, Symbol, BigInt 7 TYPES
 
 const score = 100 
+const scoreValue = 100.3
+const isLoggedIn= false
+const outsideTemp =  null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId); // false, symbols are unique
+//const bigNumber = BigInt(1234567890123456789012345678901234567890n);
+
+REFERENCE (NON 
+        RIMITIVE DATA TYPE)
+// Object, Array, Function, Date, RegExp, Map, Set 
 
 
+const heroes = ["shaktiman", "naagraj", "doga"]
 
-// REFERENCE (NON PRIMITIVE DATA TYPE)
-// Object, Array, Function, Date, RegExp, Map, Set
+let myObj ={
+    name: "Pranjal",
+    age: 19,
+    
+}
+
+
+const myFunction = function() {
+    console.log("Hello World");
+}  
+
+console.loglog(typeof bigNumber); // "bigint"
